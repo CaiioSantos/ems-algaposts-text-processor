7faa8dd25747ce4f80e579dc4a7b536cfba6d622
CAIO <caiohgs13@gmail.com>
2025-05-31 12:49:55 -0300

    New project files

---
+++ src/test/java/com/algaworks/algapost/text/processor/TextProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algapost.text.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
